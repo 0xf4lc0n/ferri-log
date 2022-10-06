c1f60afd52fc01285f7394721f011119a0a5ea78
0xf4lc0n <pmakowiecki0@gmail.com>
2022-10-06 14:01:10 +0200

    refactor: rename span for get_all_logs

--- src/server/routes/logs.rs
+++ src/server/routes/logs.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use actix_web::web;
 
-#[tracing::instrument(name = "Adding a new subscriber", skip(log_repo))]
+#[tracing::instrument(name = "Get all logs", skip(log_repo))]
 pub async fn get_all_logs(log_repo: web::Data<PgLogRepo>) -> web::Json<Vec<LogEntry>> {
     let logs = log_repo.get_all_logs().await.expect("Cannot get all logs");
     web::Json(logs)
