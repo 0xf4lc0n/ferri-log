a3b727333056d77d76dda6a6c52d820c847aa944
0xf4lc0n <pmakowiecki0@gmail.com>
2022-10-06 17:34:10 +0200

    feat: add get log by id endpoint

--- src/server/routes/logs.rs
+++ src/server/routes/logs.rs
@@ -1,11 +1,31 @@
 use crate::{
-    application::prelude::LogRepository, domain::prelude::LogEntry,
+    application::prelude::LogRepository,
+    domain::prelude::{LogEntry, RepositoryError},
     infrastructure::prelude::PgLogRepo,
 };
-use actix_web::web;
+use actix_web::{web, HttpResponse, Responder};
+use tracing::{error, info};
+use uuid::Uuid;
 
 #[tracing::instrument(name = "Get all logs", skip(log_repo))]
 pub async fn get_all_logs(log_repo: web::Data<PgLogRepo>) -> web::Json<Vec<LogEntry>> {
     let logs = log_repo.get_all_logs().await.expect("Cannot get all logs");
     web::Json(logs)
+}
+
+pub async fn get_log_by_id(
+    log_id: web::Path<Uuid>,
+    log_repo: web::Data<PgLogRepo>,
+) -> impl Responder {
+    match log_repo.get_log_by_id(*log_id).await {
+        Ok(log) => HttpResponse::Ok().json(log),
+        Err(RepositoryError::Database(sqlx::Error::RowNotFound)) => {
+            info!("Log entry with id '{}' doesn't exist", log_id);
+            HttpResponse::NotFound().finish()
+        }
+        Err(e) => {
+            error!("Cannot get log entry with id '{}'. Reason: {:?}", log_id, e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
